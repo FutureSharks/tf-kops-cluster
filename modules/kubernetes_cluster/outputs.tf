9fe12a5fe0ea44dbe46217d4b29fc7fe9be2b59e
futuresharks <futuresharks@gmail.com>
2017-06-29 17:37:05 +0200

    outputs fixes

--- modules/kubernetes_cluster/outputs.tf
+++ modules/kubernetes_cluster/outputs.tf
@@ -1,11 +1,20 @@
 output "node_sg_id" {
-  value = "${aws_security_group.master.id}"
+  value = "${aws_security_group.node.id}"
 }
 output "node_asg_name" {
   value = "${aws_autoscaling_group.node.name}"
 }
+output "node_asg_id" {
+  value = "${aws_autoscaling_group.node.id}"
+}
+output "node_asg_arn" {
+  value = "${aws_autoscaling_group.node.arn}"
+}
 output "master_sg_id" {
-  value = "${aws_security_group.node.id}"
+  value = "${aws_security_group.master.id}"
+}
+output "master_elb_sg_id" {
+  value = "${aws_security_group.master_elb.id}"
 }
 output "master_elb_dns_name" {
   value = "${aws_elb.master.dns_name}"
@@ -15,6 +24,12 @@ output "master_internal_elb_dns_name" {
 }
 output "master_asg_name" {
   value = "${aws_autoscaling_group.master.name}"
+}
+output "master_asg_id" {
+  value = "${aws_autoscaling_group.master.id}"
+}
+output "master_asg_arn" {
+  value = "${aws_autoscaling_group.master.arn}"
 }
 output "cloudwatch_log_group_name" {
   value = "${aws_cloudwatch_log_group.cluster.name}"
