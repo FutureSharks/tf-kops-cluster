5be697d7a6350c56de319bbef65298d84e4666b7
futuresharks <futuresharks@gmail.com>
2017-07-17 14:17:38 +0200

    Adding kops version check

--- README.md
+++ README.md
@@ -39,6 +39,10 @@ See comments in [modules/kubernetes_cluster/variables.tf](modules/kubernetes_clu
 
 Full example with VPC resources in [kubernetes_cluster_example.tf](kubernetes_cluster_example.tf). `terraform apply` can be run from the root of this repo to build example cluster with shared VPC and IAM resources.
 
+## Notes
+
+If the `cluster_fqdn` is not resolvable from where you run `kubectl` then you get an SSL error. The easiest solution is to use a public domain in Route53.
+
 ## Versions
 
 Currently kops version 1.6.0 is supported. When a new kops version is available and the user data scripts are changed, a tag will be created with this format: `kops_version`-`module_version`
--- modules/kubernetes_cluster/kops.tf
+++ modules/kubernetes_cluster/kops.tf
@@ -1,4 +1,11 @@
+resource "null_resource" "check_kops_version" {
+  provisioner "local-exec" {
+    command = "${path.module}/bin/check_kops_version.sh ${path.module}/data/user_data.sh"
+  }
+}
+
 resource "null_resource" "create_cluster" {
+  depends_on = [ "null_resource.check_kops_version" ]
   provisioner "local-exec" {
     command = "kops create cluster --dns ${var.dns} --networking flannel --zones=${join(",", data.aws_availability_zones.available.names)} --node-count=${var.node_asg_desired} --master-zones=${join(",", data.aws_availability_zones.available.names)} --target=terraform --api-loadbalancer-type=public --vpc=${var.vpc_id} --state=s3://${var.kops_s3_bucket_id} --kubernetes-version ${var.kubernetes_version} ${var.cluster_fqdn}"
   }
