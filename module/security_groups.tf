f067fdff6cb1f60261bb54d1d36162ce79f7dec7
Max Williams <max.williams@deliveryhero.com>
2018-01-12 12:13:11 +0100

    updating SG rules from latest kops version

--- module/security_groups.tf
+++ module/security_groups.tf
@@ -25,12 +25,30 @@ resource "aws_security_group_rule" "all_node_to_node" {
   protocol                 = "-1"
 }
 
-resource "aws_security_group_rule" "node_to_master_tcp_1-4000" {
+resource "aws_security_group_rule" "node_to_master_ipip" {
+  type                     = "ingress"
+  security_group_id        = "${aws_security_group.master.id}"
+  source_security_group_id = "${aws_security_group.node.id}"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "4"
+}
+
+resource "aws_security_group_rule" "node_to_master_tcp_1-2379" {
   type                     = "ingress"
   security_group_id        = "${aws_security_group.master.id}"
   source_security_group_id = "${aws_security_group.node.id}"
   from_port                = 1
-  to_port                  = 4000
+  to_port                  = 2379
+  protocol                 = "tcp"
+}
+
+resource "aws_security_group_rule" "node_to_master_tcp_2382-4001" {
+  type                     = "ingress"
+  security_group_id        = "${aws_security_group.master.id}"
+  source_security_group_id = "${aws_security_group.node.id}"
+  from_port                = 2382
+  to_port                  = 4001
   protocol                 = "tcp"
 }
 
