9b980bdaf25c9b42e9fa48f2c6f5665b3972fdea
Mikhail Zholobov <legal90@gmail.com>
2018-07-17 14:06:13 +0200

    Wrap kops command to multiple lines
    Just for readability & maintainability

--- module/kops.tf
+++ module/kops.tf
@@ -8,12 +8,34 @@ resource "null_resource" "create_cluster" {
   depends_on = ["null_resource.check_kops_version"]
 
   provisioner "local-exec" {
-    command = "kops create cluster --cloud=aws --dns ${var.kops_dns_mode} --authorization RBAC --networking ${var.kubernetes_networking} --zones=${join(",", data.aws_availability_zones.available.names)} --node-count=${var.node_asg_desired} --master-zones=${local.master_azs} --target=terraform --api-loadbalancer-type=public --vpc=${var.vpc_id} --ssh-public-key ${var.ssh_public_key_path} --state=s3://${var.kops_s3_bucket_id} --kubernetes-version ${var.kubernetes_version} ${local.cluster_fqdn}"
+    command = <<EOT
+      kops create cluster \
+      --cloud=aws \
+      --dns ${var.kops_dns_mode} \
+      --authorization RBAC \
+      --networking ${var.kubernetes_networking} \
+      --zones=${join(",", data.aws_availability_zones.available.names)} \
+      --node-count=${var.node_asg_desired} \
+      --master-zones=${local.master_azs} \
+      --target=terraform \
+      --api-loadbalancer-type=public \
+      --vpc=${var.vpc_id} \
+      --ssh-public-key ${var.ssh_public_key_path} \
+      --state=s3://${var.kops_s3_bucket_id} \
+      --kubernetes-version ${var.kubernetes_version} \
+      ${local.cluster_fqdn}
+    EOT
   }
 
   provisioner "local-exec" {
     when    = "destroy"
-    command = "kops delete cluster --yes --state=s3://${var.kops_s3_bucket_id} --unregister ${local.cluster_fqdn}"
+    command = <<EOT
+      kops delete cluster \
+      --state=s3://${var.kops_s3_bucket_id} \
+      --unregister \
+      --yes \
+      ${local.cluster_fqdn}
+    EOT
   }
 }
 
